ae10731860fae13f2328970dd89388ec36202142
Lisa Meyer <lisamermey@gmail.com>
2022-03-29 18:30:02 -0400

    displays remaining character count; uses

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,14 @@
-$(document).ready(() => {
-  
+$(document).ready(function() {
+  let counter = 140;
+  $('#tweet-text').on('input', function(e) {
+    let len = $(this).val().length;
+    let remainder = counter - len;
+    
+    $('.counter').text(remainder);
+    if (remainder < 0) {
+      $('.counter').css('color', 'red')
+    } else {
+      $('.counter').css('color', 'black')
+    }
+  });
 });
