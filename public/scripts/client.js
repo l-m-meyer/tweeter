a0a83e34484e7fd99af6f615cf7debcb60943340
Lisa Meyer <lisamermey@gmail.com>
2022-04-02 21:48:48 -0400

    refactored js files for best practices

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,50 +1,54 @@
 /*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+* Submits new tweets.
+*/
 
-// prevents cross-site scripting
-const escape = function(str) {
-  let div = document.createElement("div");
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-};
-
-//prepends tweet to the tweets container
-const renderTweets = (tweets) => {
-  for (let tweet of tweets) {
-    $('.tweet-container').prepend(createTweetElement(tweet));
-  }
-};
-
-// generates HTML for new tweet to be rendered
-const createTweetElement = (data) => {
-    
-  let $tweet = $(`
-  <article class="tweet">
-    <header class="tweet__header">
-      <div>
-        <img src="${data.user.avatars}"></img>
-        <h4 class="tweet--name">${data.user.name}</h4>
-      </div>
-      <h4 class="tweet--handle">${data.user.handle}</h4>
-    </header>
-    <p class="tweet--content">${escape(data.content.text)}</p>
-    <footer class="tweet__footer">
-      <output class="footer--date">${timeago.format(data.created_at)}</output>
-      <div class="footer__icons">
-        <i class="fa-solid fa-flag"></i>
-        <i class="fa-solid fa-retweet"></i>
-        <i class="fa-solid fa-heart"></i>
-      </div>
-    </footer>
-  </article>`);
-
-  return $tweet;
-};
-
-$(() => {
+(($) => {
+  $(() => {
+    loadTweets();
+    writeTweet();
+  });
+  
+  // prevents cross-site scripting
+  const escape = function(str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+  
+  //prepends tweet to the tweets container
+  const renderTweets = (tweets) => {
+    for (let tweet of tweets) {
+      $('.tweet-container').prepend(createTweetElement(tweet));
+    }
+  };
+  
+  // generates HTML for new tweet to be rendered
+  const createTweetElement = (data) => {
+      
+    let $tweet = $(`
+    <article class="tweet">
+      <header class="tweet__header">
+        <div>
+          <img src="${data.user.avatars}"></img>
+          <h4 class="tweet--name">${data.user.name}</h4>
+        </div>
+        <h4 class="tweet--handle">${data.user.handle}</h4>
+      </header>
+      <p class="tweet--content">${escape(data.content.text)}</p>
+      <footer class="tweet__footer">
+        <output class="footer--date">${timeago.format(data.created_at)}</output>
+        <div class="footer__icons">
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-heart"></i>
+        </div>
+      </footer>
+    </article>`);
+  
+    return $tweet;
+  };
+  
+  // displays tweets from database to tweets container
   const loadTweets = () => {
     $.get('/tweets').then(tweets => {
       console.log('tweets: ', tweets);
@@ -52,28 +56,28 @@ $(() => {
     });
   };
 
-  loadTweets();
-  
-  // event listener
-  $('.new-tweet--form').submit(function(e) {
-    e.preventDefault();
-    const tweet = $('#tweet-text');
-    const query = $(this).serialize();
-    tweet.on('input', () => $('#error').slideUp('slow'));
-    
-    if (!tweet.val().trim().length) {
-      $('#error').html('⛔ Nothing to say? ⛔').slideDown('slow');
-    } else if (!tweet.val().length) {
-      $('#error').html('⛔ Tweet too short! ⛔').slideDown('slow');
-    } else if (tweet.val().length > 140) {
-      $('#error').html('⛔ Tweet too long! Keep it under 140! ⛔').slideDown('slow');
-    } else {
-      $.post('/tweets', query).then(() => {
-        $('.tweet-container').empty();
-        loadTweets();
-        $('.new-tweet--form').trigger('reset');
-        $('.counter').html('140');
-      });
-    }
-  });
-});
+  // adds new tweets to database to be displayed on page
+  const writeTweet = () => {
+    $('.new-tweet--form').submit(function(e) {
+      e.preventDefault();
+
+      const tweet = $('#tweet-text');
+      const query = $(this).serialize();
+
+      tweet.on('input', () => $('#error').slideUp('slow'));
+      
+      if (!tweet.val().trim().length) {
+        $('#error').text('⛔ Don\'t be shy, write that tweet! ⛔').slideDown('slow');
+      } else if (tweet.val().length > 140) {
+        $('#error').text('⛔ Someone\'s gotta a lot to say, eh? Keep it under 140! ⛔').slideDown('slow');
+      } else {
+        $.post('/tweets', query).then(() => {
+          $('.tweet-container').empty();
+          loadTweets();
+          $('.new-tweet--form').trigger('reset');
+          $('.counter').text('140');
+        });
+      }
+    });
+  };
+})(jQuery);
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,23 @@
-$(() => {
-  $('#tweet-text').on('input', function(e) {
-    
-    let counter = 140;
-    let len = $(this).val().length;
-    let remainder = counter - len;
-    const inputCounter = $(this).next().children()[1];
-    $(inputCounter).text(remainder);
-    if (remainder < 0) {
-      $(inputCounter).css('color', 'red');
-    } else {
-      $(inputCounter).css('color', 'black');
-    }
+/**
+ * Displays remaining character counter.
+ */
+(($) => {
+  $(() => {
+    charCounter();
   });
-});
+  
+  const charCounter = () => {
+    $('#tweet-text').on('input', function(e) {
+      let counter = 140;
+      let len = $(this).val().length;
+      let remainder = counter - len;
+      const inputCounter = $(this).next().children()[1];
+      $(inputCounter).text(remainder);
+      if (remainder < 0) {
+        $(inputCounter).css('color', 'var(--red)');
+      } else {
+        $(inputCounter).css('color', 'var(--darkred)');
+      }
+    });
+  }
+})(jQuery);
